ce9b8602c0a154a20fb5f8cdde3f5203ee33d3fd
julianostroschon <julianostroschon@gmail.com>
2024-08-21 13:21:02 -0300

    ♻️ Corrige importação desnecessária

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,16 @@ import App from "./App.vue";
 // Import icon libraries
 import "@quasar/extras/material-icons/material-icons.css";
 // Import Quasar css
-import "quasar/src/css/index.sass";
 import "./assets/global.css";
 import "./style.css";
 import "uno.css";
 
-const plugins = [Quasar, villus, router, pinia];
+const app = createApp(App)
+  .use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  })
+  .use(villus)
+  .use(router)
+  .use(pinia);
 
-const app = createApp(App);
-for (const plugin of plugins) {
-  app.use(plugin);
-}
 app.mount("#app");
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,6 +2,6 @@
   <q-card
     class="row justify-evenly q-pa-md bg-primary q-gutters-sm items-center"
   >
-  <h1>Hello Vite + Vue 3</h1>
+    <h1>Hello Wolrd!</h1>
   </q-card>
 </template>
