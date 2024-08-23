8374ee60be1cac71e7db95ca5e57a964d1e60cc8
julianostroschon <julianostroschon@gmail.com>
2024-08-23 11:45:28 -0300

    🎨 Corrige codegen

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,13 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
+// console.log({env: process.env })
 
-const schema =
-  process.env.VITE_APP_ENDPOINT || 'https://scheduller.onrender.com/graphql';
+const schema = process.env.VITE_APP_ENDPOINT || 'http://0.0.0.0:30000/graphql';
 // console.log({ schema, env: process.env })
 
 const config: CodegenConfig = {
   overwrite: true,
   schema,
-  documents: [
-    'src/graphql/queries/*.graphql',
-    'src/graphql/mutations/*.graphql',
-  ],
+  documents: ['src/graphql/modules/**/*.graphql'],
   generates: {
     'src/graphql/generated/': {
       preset: 'client',
@@ -22,6 +19,18 @@ const config: CodegenConfig = {
         skipTypename: true,
       },
       plugins: [{ add: { content: '// @ts-nocheck' } }],
+    },
+    'modules/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        extension: '.gql.ts',
+        baseTypesPath: 'src/graphql/modules',
+      },
+      config: {
+        useTypeImports: true,
+        skipTypename: true,
+      },
+      plugins: ['typescript-operations', 'typed-document-node'],
     },
     // "src/graphql/generated/schema.json": {
     //   plugins: ["introspection"],
