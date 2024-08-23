5232163de05474d1c989385868da5731fbde0af0
julianostroschon <julianostroschon@gmail.com>
2024-08-23 17:02:35 -0300

    ♻️ Corrige composables

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,45 +1,32 @@
-import { TypedDocumentNode } from "@apollo/client";
-import { DocumentNode } from "graphql";
-import { useQuery, useMutation, QueryVariables, MaybeLazyOrRef } from "villus";
+import type { MaybeRef, Operation, QueryVariables } from 'villus';
 
-export async function runQuery<Output>(
-  query: string | DocumentNode | TypedDocumentNode<any, QueryVariables>,
-  variables?: MaybeLazyOrRef<QueryVariables> | undefined
-): Promise<Output> {
-  if (variables) {
-    const { data } = (await useQuery<Output>({
-      query,
-      variables,
-    }).execute());
+export async function query<T>(options: {
+  query: MaybeRef<Operation<any, QueryVariables>['query']>;
+  variables?: QueryVariables | undefined;
+}): Promise<T> {
+  try {
+    const { data } = (await useQuery(options).execute()) as { data: T };
 
-    if (data) {
-      return data;
-    }
+    const result = computed(() => data);
+    return result.value;
+  } catch (e) {
+    console.log(e);
+    return {} as T;
   }
-  const { data } = (await useQuery<Output>({
-    query,
-  }).execute()) as {
-    data: Output;
-  };
-  if (data) {
-    return data;
-  }
-  throw new Error("Error on query");
 }
 
-export async function runMutation(
-  query: string | DocumentNode | TypedDocumentNode<any, QueryVariables>,
-  variables?: MaybeLazyOrRef<QueryVariables> | undefined
-): Promise<Record<string, Record<string, string>>> {
-  if (variables) {
-    const { data } = (await useMutation(query).execute(variables)) as {
-      data: Record<string, Record<string, string>>;
+export async function mutation<T, V extends QueryVariables>(
+  mutation: Operation<T, V>['query'],
+  variables?: V,
+): Promise<T> {
+  try {
+    const { data } = (await useMutation(mutation).execute(variables)) as {
+      data: T;
     };
-    return data;
+    const result = computed(() => data);
+    return result.value;
+  } catch (e) {
+    console.log(e);
+    return {} as T;
   }
-
-  const { data } = (await useMutation(query)) as unknown as {
-    data: Record<string, Record<string, string>>;
-  };
-  return data;
 }
