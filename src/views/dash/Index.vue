ed1eeb8dba654f944d6d5c8e666bd20cbb9801a5
julianostroschon <julianostroschon@gmail.com>
2024-08-23 17:03:33 -0300

    📝 Define rota de testes

---
+++ src/views/dash/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    {{ user }}
+  </div>
+</template>
+<script lang="ts">
+import type { UserQuery } from '~types';
+
+const result = await query<UserQuery>({ query: UserDocument });
+
+export default defineComponent({
+  name: 'Index',
+  setup() {
+    return {
+      user: result?.user,
+    };
+  },
+});
+</script>
